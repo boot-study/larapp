12be5466c3c45461f93145faaf8128d954cdbc0e
boot-study <voyasa9172@latovic.com>
2021-11-26 11:47:20 +0500

    admin module created

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h2>Home</h2>
+@endsection
---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <title>@yield('title', 'Laravel')</title>
+</head>
+<body>
+
+<div class="container">
+    @include('admin.layouts.includes.navbar')
+    @yield('content')
+</div>
+
+</body>
+</html>
